de66846e826c91bcbb6a5427ba97d95dc0b236d0
xxsimoxx <simone@gieffeedizioni.it>
2025-08-06 09:21:53 +0200

    Add script to update languages

---
+++ bin/update-languages.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ ! -f "limit-login-attempts-ng.php" ]; then
+	echo "This script must be called in the main plugin directory."
+	exit 1
+fi
+
+if [ ! -d "languages" ]; then
+	echo "Missing languages directory."
+	exit 1
+fi
+
+wp i18n make-pot . languages/limit-login-attempts.pot
+
+pushd languages >/dev/null
+
+for po in limit-login-attempts-*.po; do
+	mo=${po%.po}.mo
+	msgmerge --sort-output --update --backup=simple $po limit-login-attempts.pot
+	msgfmt $po -o $mo
+	echo -n "Updated "
+	echo $po | sed -nE 's/.*-([a-z]{2}_?[A-Z]?[A-Z]?).po$/\1/p'
+done
+
+popd >/dev/null
+
+echo "All done."
+
+
