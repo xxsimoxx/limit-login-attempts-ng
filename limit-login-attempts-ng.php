2032bdb16fb7a374c7538ce1cea764b2db30b72b
xxsimoxx <simone@gieffeedizioni.it>
2025-09-08 08:59:26 +0200

    Fix headers

--- limit-login-attempts-ng.php
+++ limit-login-attempts-ng.php
@@ -1,36 +1,36 @@
 <?php
 /*
-	Plugin Name:  Limit Login Attempts NG
-	Description:  Limit rate of login attempts, including by way of cookies, for each IP.
-	Author:       Simone Fioravanti
-	Author URI:   https://simonefioravanti.it
-	Text Domain:  limit-login-attempts
-	Domain Path:  /languages
-	Version:      1.0.0
-	Requires CP:  2.0
-	Requires PHP: 7.4
-
-
-	This plugin is a fork of Limit Login Attempts Copyright 2008 - 2012 Johan Eenfeldt
-	Copyright 2008 - 2012 Johan Eenfeldt
-	Thanks to Michael Skerwiderski for reverse proxy handling suggestions.
-
-	Licenced under the GNU GPL:
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * Plugin Name:  Limit Login Attempts NG
+ * Description:  Limit rate of login attempts, including by way of cookies, for each IP.
+ * Author:       Simone Fioravanti
+ * Author URI:   https://simonefioravanti.it
+ * Text Domain:  limit-login-attempts
+ * Domain Path:  /languages
+ * Version:      1.0.0
+ * Requires CP:  2.0
+ * Requires PHP: 7.4
+ *
+ *
+ * This plugin is a fork of Limit Login Attempts Copyright 2008 - 2012 Johan Eenfeldt
+ * Copyright 2008 - 2012 Johan Eenfeldt
+ * Thanks to Michael Skerwiderski for reverse proxy handling suggestions.
+ *
+ * Licenced under the GNU GPL:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 namespace xxsimoxx\LimitLoginAttemptsNG;
 
